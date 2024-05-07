e5e242349ed3154554bbe0975a50df7a1b861c9c
Nourena Nabil <nourenanabil@Karims-MacBook-Air.local>
2024-05-07 22:05:44 +1000

    Changed execute function so that top window listen to event

--- widget/widget.js
+++ widget/widget.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 // Write your module here
 // It must send an event "frames:loaded" from the top frame containing a list of { name:label } pairs,
 // which describes all the fields in each frame.
@@ -7,33 +7,32 @@
 
 //Add scrapeFields function to access fields within a form
 function scrapeFields() {
-try{
-  const form = window.document.querySelector("form");
-  console.log("Form content", form)
-
-} catch (error) {
+  try {
+    const form = window.document.querySelector("form");
+    console.log("Form content", form);
+  } catch (error) {
     console.error("Error scraping fields:", error);
   }
 }
 
 function execute() {
-	try {
+  try {
     // Step 1 Scrape Fields and Create Fields list object.
     // Step 2 Add Listener for Top Frame to Receive Fields.
 
-    scrapeFields()
-    
+    scrapeFields();
+
     if (isTopFrame()) {
-      window.addEventListener('message', (event) => {
+      window.top.addEventListener("message", (event) => {
         // - Merge fields from frames.
         // - Process Fields and send event once all fields are collected.
       });
     } else if (!isTopFrame()) {
       // Child frames sends Fields up to Top Frame.
     }
-	} catch (e) {
-		console.error(e)
-	}
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 execute();
@@ -46,5 +45,5 @@ function getTopFrame() {
 }
 
 function isTopFrame() {
-  return window.location.pathname == '/context.html';
+  return window.location.pathname == "/context.html";
 }
