2e5499426febe0d352a6e85b0824fa5d84d06014
Nourena Nabil <nourenanabil@Karims-MacBook-Air.local>
2024-05-11 20:48:18 +1000

    Refactored widget file structure to use class-based implementation and updated related tests

--- test/get_fields_test.js
+++ test/get_fields_test.js
@@ -1,55 +1,56 @@
 const expect = chai.expect;
 
 expectation = [
-  { "address_line_1" : "Address Line 1" },
-  { "cc_number" : "Number" },
-  { "cc_type" : "Type" },
-  { "country" : "Country" },
-  { "first_name" : "First Name" },
-  { "last_name" : "Last Name" }
+  { address_line_1: "Address Line 1" },
+  { cc_number: "Number" },
+  { cc_type: "Type" },
+  { country: "Country" },
+  { first_name: "First Name" },
+  { last_name: "Last Name" },
 ];
 
-describe('Widget #getFields', () => {
-
+describe("Widget #getFields", () => {
   before((done) => {
-    fixture.setBase('assets');
-    fixture.load('top.html');
+    fixture.setBase("assets");
+    fixture.load("top.html");
 
-    document.addEventListener('frames:loaded', (event) => {
-      this.result = event.detail.fields
+    document.addEventListener("frames:loaded", (event) => {
+      this.result = event.detail.fields;
       done();
     });
   });
 
-  it('should extract the fields', () => {
+  it("should extract the fields", () => {
     console.log(this.result);
     expect(this.result).to.deep.equal(expectation);
   });
 });
 
-describe("Widget #scrapeFields", () =>{
+describe("Widget #scrapeFields", () => {
+  it("should return an empty array if no form is found", function () {
+    document.body.innerHTML = "<h1>Hello</h1>";
+    expect(frameManager.scrapeFields()).to.deep.equal([]);
+  });
 
-  it('should return an empty array if no form is found', function() {
-    document.body.innerHTML = '<h1>Hello</h1>';
-    expect(scrapeFields()).to.deep.equal([]);
+  it("should return an empty array if form has no input, select, or textarea fields", function () {
+    document.body.innerHTML = "<form></form>";
+    expect(frameManager.scrapeFields()).to.deep.equal([]);
   });
-  
-  it('should return an empty array if form has no input, select, or textarea fields', function() {
-    document.body.innerHTML = '<form></form>';
-    expect(scrapeFields()).to.deep.equal([]);
+
+  it("should return an array with one object if form has only one input element", function () {
+    document.body.innerHTML =
+      '<form><label for="testName">Test Label</label><input id="testName" name="testName"></form>';
+    expect(frameManager.scrapeFields()).to.deep.equal([
+      { testName: "Test Label" },
+    ]);
   });
-  
-  it('should return an array with one object if form has only one input element', function() {
-    document.body.innerHTML = '<form><label for="testName">Test Label</label><input id="testName" name="testName"></form>';
-    expect(scrapeFields()).to.deep.equal([{ "testName": 'Test Label' }]);
+
+  it("should return an empty array if form has elements other than input, select, or textarea", function () {
+    document.body.innerHTML = "<form><div></div></form>";
+    expect(frameManager.scrapeFields()).to.deep.equal([]);
   });
-  
-  it('should return an empty array if form has elements other than input, select, or textarea', function() {
-    document.body.innerHTML = '<form><div></div></form>';
-    expect(scrapeFields()).to.deep.equal([]);
-  });
-  
-  it('should return an array of objects with name and label pairs for each input element', function() {
+
+  it("should return an array of objects with name and label pairs for each input element", function () {
     document.body.innerHTML = `
     <form>
       <label for="name1">Label 1</label>
@@ -59,79 +60,80 @@ describe("Widget #scrapeFields", () =>{
       <label for="name3">Label 3</label>
       <input id="name3" name="name3">
     </form>
-  `;  expect(scrapeFields()).to.deep.equal([
-      { name1: 'Label 1' },
-      { name2: 'Label 2' },
-      { name3: 'Label 3' }
+  `;
+    expect(frameManager.scrapeFields()).to.deep.equal([
+      { name1: "Label 1" },
+      { name2: "Label 2" },
+      { name3: "Label 3" },
     ]);
   });
-})
+});
 
-describe('Widget #sortByNameAscending', () => {
-  it('should return an empty array if input array is empty', () => {
-    const result = sortByNameAscending([]);
-    expect(result).to.be.an('array').that.is.empty;
+describe("Widget #sortFieldsByNameAscending", () => {
+  it("should return an empty array if input array is empty", () => {
+    const result = frameManager.sortFieldsByNameAscending([]);
+    expect(result).to.be.an("array").that.is.empty;
   });
 
-  it('should return the same array if it has only one element', () => {
-    const input = [{ 'first_name': 'First Name' }];
-    const result = sortByNameAscending(input);
+  it("should return the same array if it has only one element", () => {
+    const input = [{ first_name: "First Name" }];
+    const result = frameManager.sortFieldsByNameAscending(input);
     expect(result).to.deep.equal(input);
   });
 
-  it('should sort an array of objects by name in ascending order', () => {
+  it("should sort an array of objects by name in ascending order", () => {
     const input = [
-      { 'last_name': 'Last Name' },
-      { 'first_name': 'First Name' },
-      { 'country': 'Country' },
-      { 'address_line_1': 'Address Line 1' }
+      { last_name: "Last Name" },
+      { first_name: "First Name" },
+      { country: "Country" },
+      { address_line_1: "Address Line 1" },
     ];
     const expected = [
-      { 'address_line_1': 'Address Line 1' },
-      { 'country': 'Country' },
-      { 'first_name': 'First Name' },
-      { 'last_name': 'Last Name' }
+      { address_line_1: "Address Line 1" },
+      { country: "Country" },
+      { first_name: "First Name" },
+      { last_name: "Last Name" },
     ];
-    const result = sortByNameAscending(input);
+    const result = frameManager.sortFieldsByNameAscending(input);
     expect(result).to.deep.equal(expected);
   });
 
-  it('should handle arrays with objects having duplicate names', () => {
+  it("should handle arrays with objects having duplicate names", () => {
     const input = [
-      { 'last_name': 'Last Name' },
-      { 'first_name': 'First Name' },
-      { 'country': 'Country' },
-      { 'address_line_1': 'Address Line 1' },
-      { 'last_name': 'Another Last Name' }
+      { last_name: "Last Name" },
+      { first_name: "First Name" },
+      { country: "Country" },
+      { address_line_1: "Address Line 1" },
+      { last_name: "Another Last Name" },
     ];
     const expected = [
-      { 'address_line_1': 'Address Line 1' },
-      { 'country': 'Country' },
-      { 'first_name': 'First Name' },
-      { 'last_name': 'Another Last Name' },
-      { 'last_name': 'Last Name' }
+      { address_line_1: "Address Line 1" },
+      { country: "Country" },
+      { first_name: "First Name" },
+      { last_name: "Another Last Name" },
+      { last_name: "Last Name" },
     ];
-    const result = sortByNameAscending(input);
+    const result = frameManager.sortFieldsByNameAscending(input);
     expect(result).to.deep.equal(expected);
   });
 });
 
-describe('Widget #countFrames', () => {
-  it('should return 0 for a window with no frames', () => {
+describe("Widget #countFrames", () => {
+  it("should return 0 for a window with no frames", () => {
     const windowObj = { frames: [] };
-    const result = countFrames(windowObj);
+    const result = frameManager.countFrames(windowObj);
     expect(result).to.equal(0);
   });
 
-  it('should return the correct count for a window with frames', () => {
+  it("should return the correct count for a window with frames", () => {
     const windowObj = {
       frames: [
         { frames: [] }, // One frame with no sub-frames
         { frames: [{}, {}] }, // One frame with two sub-frames
-        {} // One frame with no sub-frames
-      ]
+        {}, // One frame with no sub-frames
+      ],
     };
-    const result = countFrames(windowObj);
+    const result = frameManager.countFrames(windowObj);
     expect(result).to.equal(5); // Total frames: 1 + 2 + 2 = 5
   });
 });
--- widget/widget.js
+++ widget/widget.js
@@ -1,86 +1,97 @@
 "use strict";
 
-const TOP_FRAME_LOCATION = "/context.html";
-const TOP_WINDOW_URL = "http://localhost:9999/?id=25360429";
-const INPUT_FIELDS = ["input", "select", "textarea"];
-
-function getTopFrame() {
-  return window.top.frames[0];
-}
-
-function isTopFrame() {
-  return window.location.pathname === TOP_FRAME_LOCATION;
-}
-
-const sortByNameAscending = (array) => {
-  return array.slice().sort((a, b) => {
-    const keyA = Object.keys(a)[0];
-    const keyB = Object.keys(b)[0];
-    return keyA.localeCompare(keyB) || a[keyA].localeCompare(b[keyB]);
-  });
-};
-
-function countFrames(windowObj) {
-  if (!windowObj) {
-    return 0;
-  }
-  let count = 0;
-
-  for (let i = 0; i < windowObj?.frames?.length; i++) {
-    count++;
-    count += countFrames(windowObj?.frames[i]);
+class FrameManager {
+  constructor({
+    topFrameLocation,
+    topWindowURL,
+    inputFields = ["input", "select", "textarea"],
+  }) {
+    this.topFrameLocation = topFrameLocation;
+    this.topWindowURL = topWindowURL;
+    this.inputFields = inputFields;
+    this.totalFrames = this.countFrames(window.top);
   }
 
-  return count;
-}
+  getTopFrame() {
+    return window.top.frames[0];
+  }
 
-function scrapeFields() {
-  try {
-    const form = window.document.querySelector("form");
-    if (!form) {
-      return [];
-    }
+  isTopFrame() {
+    return window.location.pathname === this.topFrameLocation;
+  }
 
-    const inputs = form.querySelectorAll(INPUT_FIELDS.join(", "));
-    return Array.from(inputs).map(({ id, name }) => {
-      const labelElement = form.querySelector(`label[for="${id}"]`);
-      return { [name]: labelElement?.textContent };
+  sortFieldsByNameAscending(array) {
+    return array.slice().sort((a, b) => {
+      const keyA = Object.keys(a)[0];
+      const keyB = Object.keys(b)[0];
+      return keyA.localeCompare(keyB) || a[keyA].localeCompare(b[keyB]);
     });
-  } catch (error) {
-    console.error("Error scraping fields:", error);
   }
-}
 
-function execute(totalFrames) {
-  try {
-    const fields = scrapeFields();
+  countFrames(windowObj) {
+    let count = 0;
 
-    if (!isTopFrame()) {
-      getTopFrame().postMessage(fields, TOP_WINDOW_URL);
-      return;
+    for (let i = 0; i < windowObj?.frames?.length; i++) {
+      count++;
+      count += this.countFrames(windowObj?.frames[i]);
     }
 
-    let mergedFields = [];
-    mergedFields.push(...fields);
-    let countFrames = 0;
+    return count;
+  }
 
-    getTopFrame().addEventListener("message", ({ data }) => {
-      console.log("Received fields from child frame:", data);
-      countFrames++;
-      mergedFields.push(...data);
-
-      if (countFrames === totalFrames) {
-        const sortedFields = sortByNameAscending(mergedFields);
-        const framesLoadedEvent = new CustomEvent("frames:loaded", {
-          detail: { fields: sortedFields },
-        });
-        window.document.dispatchEvent(framesLoadedEvent);
+  scrapeFields() {
+    try {
+      const form = window.document.querySelector("form");
+      if (!form) {
+        return [];
       }
-    });
-  } catch (e) {
-    console.error(e);
+
+      const inputs = form.querySelectorAll(this.inputFields.join(", "));
+      return Array.from(inputs).map(({ id, name }) => {
+        const labelElement = form.querySelector(`label[for="${id}"]`);
+        return { [name]: labelElement?.textContent };
+      });
+    } catch (error) {
+      console.error("Error scraping fields:", error);
+    }
+  }
+
+  execute() {
+    try {
+      const fields = this.scrapeFields();
+
+      if (!this.isTopFrame()) {
+        this.getTopFrame().postMessage(fields, this.topWindowURL);
+        return;
+      }
+
+      let countFrames = 0;
+      let mergedFields = [];
+
+      mergedFields.push(...fields);
+
+      this.getTopFrame().addEventListener("message", ({ data }) => {
+        console.log("Received fields from child frame:", data);
+        countFrames++;
+        mergedFields.push(...data);
+
+        if (countFrames === this.totalFrames) {
+          const sortedFields = this.sortFieldsByNameAscending(mergedFields);
+          const framesLoadedEvent = new CustomEvent("frames:loaded", {
+            detail: { fields: sortedFields },
+          });
+          window.document.dispatchEvent(framesLoadedEvent);
+        }
+      });
+    } catch (e) {
+      console.error(e);
+    }
   }
 }
 
-const totalFrames = countFrames(window.top);
-execute(totalFrames);
+const frameManager = new FrameManager({
+  topFrameLocation: "/context.html",
+  topWindowURL: "http://localhost:9999/?id=25360429",
+});
+
+frameManager.execute();
